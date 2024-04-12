262c55e89928156dcd531d68f58ff6e938f04b57
SilverMoon <suhongbo0725@gmail.com>
2024-04-12 17:20:44 +0800

    怪

--- GameEvents/GEM.cs
+++ GameEvents/GEM.cs
@@ -1,4 +1,6 @@
-﻿namespace ProgressSystem.GameEvents;
+﻿using System.Xml.Schema;
+
+namespace ProgressSystem.GameEvents;
 
 public static class GEM
 {
@@ -12,8 +14,48 @@ public static class GEM
         }
         return _events.TryAdd(uniqueLabel, gameEvent);
     }
-    public static bool TryGet(string uniqueLabel, out GameEvent gameEvent)
+    public static bool TryGet(string uniqueLabel, out GameEvent? gameEvent)
     {
         return _events.TryGetValue(uniqueLabel, out gameEvent);
+    }
+    public static TagCompound? Save(GameEvent gameEvent)
+    {
+        TagCompound? tag = null;
+        if (gameEvent is ISaveable sge)
+        {
+            tag = new()
+            {
+                ["Type"] = gameEvent.GetType().FullName
+            };
+            TagCompound data = [];
+            sge.Save(data);
+            tag["data"] = data;
+        }
+        return tag;
+    }
+    public static GameEvent? Load(TagCompound tag)
+    {
+        GameEvent? ge = null;
+        try
+        {
+            if (tag.TryGet("Type", out string fullName) && tag.TryGet("data", out TagCompound data))
+            {
+                GameEvent? e = ModContent.GetContent<GameEvent>().FirstOrDefault(e => e?.GetType().FullName == fullName && e is ISaveable, null);
+                if (e is not null)
+                {
+                    ge = (GameEvent?)Activator.CreateInstance(e.GetType());
+                    if (ge is not null)
+                    {
+                        ((ISaveable)ge).Load(data);
+                        return ge;
+                    }
+                }
+            }
+        }
+        catch
+        {
+            ge = null;
+        }
+        return ge;
     }
 }
