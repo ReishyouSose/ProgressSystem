2e39e5168c3aa1c2ec68059d49131de68e08696d
TigerChenzzz <1139719265@qq.com>
2024-04-13 21:20:05 +0800

    TigerUtils bug修复

--- TheUtils/TigerUtils.cs
+++ TheUtils/TigerUtils.cs
@@ -3614,7 +3614,7 @@ public static partial class TigerExtensions
     public static int RemoveAll<T>(this IList<T> list, Func<T, bool> predicate)
     {
         int result = 0;
-        for (int i = list.Count; i >= 0; --i)
+        for (int i = list.Count - 1; i >= 0; --i)
         {
             if (predicate(list[i]))
             {
