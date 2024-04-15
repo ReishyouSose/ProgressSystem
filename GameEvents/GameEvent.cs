10456741c44e51297a190586e3857a133e2b3314
TigerChenzzz <1139719265@qq.com>
2024-04-15 11:00:56 +0800

    Update GameEvent.cs

--- GameEvents/GameEvent.cs
+++ GameEvents/GameEvent.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using ProgressSystem.Core;
 
 namespace ProgressSystem.GameEvents;
 
+[Obsolete($"使用{nameof(Requirement)}代替, 部分功能则在{nameof(Achievement)}中, 使用{nameof(Achievement)}.{nameof(Achievement.Requirements)}获得成就的所有条件")]
 public abstract class GameEvent : ILoadable
 {
+    [Obsolete($"使用{nameof(Requirement)}.{nameof(Requirement.Completed)}")]
     public virtual bool IsCompleted { get; protected set; }
 
     public event Action<GameEvent>? OnCompleted;
@@ -21,6 +24,7 @@ public abstract class GameEvent : ILoadable
     {
         yield break;
     }
+    [Obsolete($"使用{nameof(Achievement)}.{nameof(Achievement.Texture)}获取, 并且在绘制时使用{nameof(Texture2DGetter)}.{nameof(Texture2DGetter.Value)}获得它的值")]
     public virtual (Texture2D?, Rectangle?) DrawData() => (null, null);
 
     public virtual void Load(Mod mod) { }
